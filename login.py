cba1cfc6495ccb5ed40c2f0f8e1bd5cae55a777f
Aaliyah Jardien <aaliyahjar13@gmail.com>
2021-06-12 14:05:41 +0200

    first commit

---
+++ login.py
@@ -0,0 +1,29 @@
+# IMPORTING TKINTER
+from tkinter import *
+from tkinter import messagebox
+
+# WINDOW FEATURES
+window = Tk()
+window.title("Login Details")
+window.geometry("400x400")
+window.resizable(0, 0)
+
+# DEFINING FUNCTIONS
+usernames = ["Aaliyah", "Karabo", "Uthmaan", "Lerato"]
+passwords = ["1111", "2222", "3333", "4444"]
+found = False
+
+# def user():
+
+    # for x in range(len(passwords)):
+
+
+# FRAME
+
+# IMAGE
+#img = PhotoImage(file="login-pic.png")
+# LABELS
+# ENTRIES
+# BUTTONS
+
+window.mainloop()
